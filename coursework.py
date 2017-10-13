bce611094c7e3b044eae8f4185b166da3ca7cd9d
David Stoddart <40209068@live.napier.ac.uk>
2017-10-13 14:05:08 +0100

    Added a initial template for guitars as to save time adding new kinds

--- coursework.py
+++ coursework.py
@@ -7,11 +7,15 @@ def root():
 
 @app.route('/strat/')
 def strat():
-    return render_template('home.html'), 200
+    return render_template('guitarmastertemplate.html'), 200
 
 @app.route('/les-paul/')
 def lespaul():
-    return render_template('home.html'), 200
+    return render_template('lespaul.html'), 200
+
+@app.route('/fly-v/')
+def flyv():
+    return render_template('flyv.html'), 200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
