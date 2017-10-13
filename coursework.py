bf7239e5db6ae5bf9d472f2c8ce9aed69eb08d13
David Stoddart <40209068@live.napier.ac.uk>
2017-10-13 15:53:05 +0100

    made it one template for all guitars and add inheritance

--- coursework.py
+++ coursework.py
@@ -5,17 +5,21 @@ app = Flask(__name__)
 def root():
     return render_template('home.html'), 200
 
-@app.route('/strat/')
-def strat():
-    return render_template('guitarmastertemplate.html'), 200
+@app.route('/<name>/')
+def guitar(name=None):
+    return render_template('{0}.html'.format(name), name=name)
 
-@app.route('/les-paul/')
-def lespaul():
-    return render_template('lespaul.html'), 200
+#@app.route('/strat/')
+#def strat():
+#    return render_template('guitarmastertemplate.html'), 200
 
-@app.route('/fly-v/')
-def flyv():
-    return render_template('flyv.html'), 200
+#@app.route('/les-paul/')
+#def lespaul():
+#    return render_template('lespaul.html'), 200
+
+#@app.route('/fly-v/')
+#def flyv():
+#    return render_template('flyv.html'), 200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
