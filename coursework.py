13f49da0ff40c8c742e02650aaa30edadb9efbc4
David Stoddart <40209068@live.napier.ac.uk>
2017-10-17 13:14:37 +0100

    Added new guitars and started on creators

--- coursework.py
+++ coursework.py
@@ -1,25 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return render_template('home.html'), 200
+    return render_template('home1.html'), 200
 
-@app.route('/<name>/')
+@app.route('/<name>/', methods=['GET', 'POST'])
 def guitar(name=None):
     return render_template('{0}.html'.format(name), name=name)
-
-#@app.route('/strat/')
-#def strat():
-#    return render_template('guitarmastertemplate.html'), 200
-
-#@app.route('/les-paul/')
-#def lespaul():
-#    return render_template('lespaul.html'), 200
-
-#@app.route('/fly-v/')
-#def flyv():
-#    return render_template('flyv.html'), 200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
