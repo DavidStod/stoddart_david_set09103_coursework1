f824d2cee03041b0b20b8b919f845eef55e08a3e
David Stoddart <40209068@live.napier.ac.uk>
2017-10-19 14:03:03 +0100

    Commented code to easier understand later

--- coursework.py
+++ coursework.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+#open the home page
 @app.route('/')
 def root():
     return render_template('home1.html'), 200
 
+#open everyother page by reading it from the url
 @app.route('/<name>/', methods=['GET', 'POST'])
 def guitar(name=None):
-    return render_template('{0}.html'.format(name), name=name)
+    return render_template('{0}.html'.format(name), name=name), 200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
